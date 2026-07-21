c44d322db8ee9e4ca032e11a7b92d8a81bf377ff
luisrcsb <luisrcsb@gmail.com>
2026-07-21 15:08:27 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 // Banco de dados dinâmico de telemetria - Krathus Racing
 
-// Verifica se existem sessões novas enviadas pelo painel secreto de upload
 const savedSessions = JSON.parse(localStorage.getItem('krathus_db_sessions'));
 
 const database = {
@@ -11,9 +10,7 @@ const database = {
             date: "17/07/2026 23:39:19",
             type: "Corrida",
             event: "Etapa Oficial",
-            laps: [
-                // Insira as voltas padrão aqui se necessário, ou deixe gerenciado pelo upload
-            ]
+            laps: []
         }
     ]
 };
