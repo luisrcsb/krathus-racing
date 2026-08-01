aa7397f5b4ef2f5491399f2f60b90da80786d2f1
luisrcsb <luisrcsb@gmail.com>
2026-08-01 04:43:44 -0300

    Create banco-de-dados.js

---
+++ banco-de-dados.js
@@ -0,0 +1,61 @@
+const listaJsonsCache = [
+    {
+        "bateriaKey": "bat_31_07_26_bateria_8",
+        "sessao": "31-07-26 Bateria (8)",
+        "id": 1785557785,
+        "nome": "31-07-26 Bateria (8).json",
+        "data": "01/08/2026",
+        "tamanho": "2.5 KB",
+        "dados": [
+            {
+                "pos": "1º",
+                "piloto": "Ronaldo",
+                "bateriaKey": "bat_31_07_26_bateria_8",
+                "sessao": "31-07-26 Bateria (8)",
+                "voltasTotais": 20,
+                "tempoTotal": "04:05,176",
+                "melhorVoltaVal": 9.856,
+                "melhorVoltaTxt": "09,856s",
+                "mediaVal": 12.259,
+                "mediaTxt": "12,259s",
+                "desvioVal": 2.641,
+                "desvio": "±2,641s",
+                "badgeColor": "#ffb703",
+                "laps": [14.661, 10.775, 10.826, 10.711, 12.210, 10.326, 10.367, 14.737, 21.240, 10.783, 15.060, 13.513, 12.885, 10.580, 9.856, 12.243, 10.970, 11.310, 11.131, 10.992]
+            },
+            {
+                "pos": "2º",
+                "piloto": "DJ",
+                "bateriaKey": "bat_31_07_26_bateria_8",
+                "sessao": "31-07-26 Bateria (8)",
+                "voltasTotais": 20,
+                "tempoTotal": "04:19,534",
+                "melhorVoltaVal": 9.944,
+                "melhorVoltaTxt": "09,944s",
+                "mediaVal": 12.977,
+                "mediaTxt": "12,977s",
+                "desvioVal": 4.277,
+                "desvio": "±4,277s",
+                "badgeColor": "#2ec4b6",
+                "laps": [9.944, 11.091, 10.276, 20.979, 10.199, 22.164, 10.630, 10.677, 10.130, 13.986, 12.778, 11.057, 12.463, 10.529, 11.160, 10.807, 14.026, 11.022, 11.410, 24.206]
+            },
+            {
+                "pos": "3º",
+                "piloto": "Raphael",
+                "bateriaKey": "bat_31_07_26_bateria_8",
+                "sessao": "31-07-26 Bateria (8)",
+                "voltasTotais": 17,
+                "tempoTotal": "03:45,814",
+                "melhorVoltaVal": 10.319,
+                "melhorVoltaTxt": "10,319s",
+                "mediaVal": 13.283,
+                "mediaTxt": "13,283s",
+                "desvioVal": 3.390,
+                "desvio": "±3,390s",
+                "badgeColor": "#e63946",
+                "laps": [11.177, 23.734, 14.951, 11.697, 11.446, 14.511, 10.319, 11.912, 11.911, 12.285, 12.312, 13.213, 11.377, 11.975, 10.870, 12.984, 19.140]
+            }
+        ]
+    }
+    // Adicione mais baterias aqui se quiser
+];
