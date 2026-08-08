2725f47acb0a4d298f6cb5b7a133c786f32be4b9
luisrcsb <luisrcsb@gmail.com>
2026-08-08 01:45:21 -0300

    Update and rename Uploads.js to upload.html

--- Uploads.js
+++
@@ -1,6 +0,0 @@
-console.log("Banco de dados integrado e pronto para sincronização com o Firebase.");
-
-// Inicializa o cache global de dados caso ainda não exista
-if (typeof listaJsonsCache === 'undefined') {
-    var listaJsonsCache = [];
-}
